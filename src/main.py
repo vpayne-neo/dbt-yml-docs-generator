664898be78b42a8a307c3c80b439cb50150a72b7
vpayne-neo <vince.payne@neofinancial.com>
2023-04-18 15:53:59 -0600

    Finished

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import yaml
+import os
 import sys
 
 
@@ -8,20 +9,14 @@ def main():
 
     with open(filePath, "r") as file:
         ymlData = yaml.safe_load(file)
-
-
-    models = []
+    
     version = ymlData["version"]
-    # print(version)
+    
+    directoryToJoin = "/Users/vincepayne/neo-analytics/packages/dbt/models/marts/cx"
 
     for i in ymlData["models"]:
-       
-        with open(i["name"] + '.yml', 'w') as ymlFile:
-                # yaml.dump(i, ymlFile)
-                print(ymlFile)
-        
-        
-    # print(models)
+        with open(os.path.join(directoryToJoin, i["name"] + '.yml'), 'w') as ymlFile:
+                yaml.dump({"version": version, "models": i }, stream=ymlFile, sort_keys=False)
 
 if __name__ == "__main__":
     main()
